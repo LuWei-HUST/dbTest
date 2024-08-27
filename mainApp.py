b86e3c4332a06aa5d01df9d76480aa1d1ac26204
luwei <1261579359@qq.com>
2024-08-27 21:42:16 +0800

    update parser

--- mainApp.py
+++ mainApp.py
@@ -3,46 +3,11 @@ import re
 import shutil
 import table
 import copy
-import parser
+from parser import SqlParser
 
 tablePathBase = "/home/luwei/code/dbTest/storage"
 curTable = "default"
 tDict = {}
-
-def createDB(dbName):
-    pass
-
-def createTable(tbName, colNames):
-    tableDirPath = os.path.join(tablePathBase, tbName)
-
-    if os.path.exists(tableDirPath):
-        print("table {} already exixts".format(tbName))
-        return False
-    else:
-        os.mkdir(tableDirPath)
-        tableMetaPath = os.path.join(tableDirPath, tbName+".meta")
-        with open(tableMetaPath, "w") as f:
-            f.writelines([i+"\n" for i in colNames])
-
-        for c in colNames:
-            colName = c.split(" ")[0]
-            colPath = os.path.join(tableDirPath, colName+".wcol")
-            with open(colPath, "w") as f:
-                pass
-
-        return True
-
-def parseCsv(csvPath, sep):
-    pass
-
-def dropTable(tbName):
-    tableDirPath = os.path.join(tablePathBase, tbName)
-    if os.path.exists(tableDirPath):
-        shutil.rmtree(tableDirPath)
-
-        print("TABLE {} DROPED".format(tbName))
-    else:
-        print("TABLE {} NOT EXISTS".format(tbName))
 
 def insert():
     tablePath = os.path.join(tablePathBase, curTable+".wtbl")
@@ -93,9 +58,8 @@ def insert():
         print("TABLE {} NOT EXISTS".format(curTable))
 
 if __name__ == "__main__":
-    select_pat = r"[ ]*select[ ]+([1-9a-zA-Z,_ \*]+)[ ]+from[ ]+([1-9a-zA-Z_]+)[ ]*;"
-    create_pat = r"[ ]*create[ ]+table[ ]+([1-9a-zA-Z_]+)\(([1-9a-zA-Z, _]+)\)[ ]*;"
-    drop_pat = r"[ ]*drop[ ]+table[ ]+([1-9a-zA-Z_]+)[ ]*;"
+
+    sqlparser = SqlParser()
 
     while True:
         ch = input("wsql#: ")
@@ -103,69 +67,6 @@ if __name__ == "__main__":
         if ch == "quit":
             exit()
 
-        # if ch == "create db":
-        #     dbName = input("enter db name: ")
-        #     r = createDB(dbName)
-        #     if r:
-        #         print("CREATE DATABASE {}".format(dbName))
-        #     else:
-        #         print("CREATE DATABASE FAILED")
-        res = re.search(select_pat, ch)
-        if res:
-            colNamesText = res.group(1)
-            colNames = colNamesText.strip().split(",")
-            colNames = [i.strip() for i in colNames]
-            # print(colNames)
-            tbName = res.group(2)
-            # print(tbName)
-            tmpT = parser.getColumn(tbName, colNames)
-            if tmpT:
-                tmpT.showTable()
-        
-        res = re.search(create_pat, ch)
-        if res:
-            tbName = res.group(1)
-            colNames = res.group(2).strip().split(",")
-            colNames = [i.strip() for i in colNames]
-            tmpCols = colNames[:]
-            colNames = []
-            for c in tmpCols:
-                tmp_ = c.split(" ")
-                tmp_ = [i.strip() for i in tmp_]
-                tmp_ = list(filter(lambda x: x and x.strip(), tmp_))
-                tmp_ = " ".join(tmp_)
-                colNames.append(tmp_)
-            r = createTable(tbName, colNames)
-            if r:
-                print("CREATE TABLE {}".format(tbName))
-            else:
-                print("CREATE DATABASE FAILED")
-
-        if ch.startswith("copy from"):
-            args = ch.split(" ")
-
-            if len(args) != 4:
-                print("syntax error")
-            else:
-                csvPath = args[-2]
-                sep = args[-1]
-                parseCsv(csvPath, sep)
-
-        res = re.search(drop_pat, ch)
-        if res:
-            tbName = res.group(1)
-            dropTable(tbName)
-
-        if ch == "use table":
-            tbName = input("enter table name: ")
-            tablePath = os.path.join(tablePathBase, tbName+".wtbl")
-            if os.path.exists(tablePath):
-                curTable = tbName
-            else:
-                print("TABLE {} NOT EXISTS".format(tbName))
-
-        if ch == "insert":
-            insert()
-
+        sqlparser.parse(ch)
 
         
--- parser.py
+++ parser.py
@@ -1,34 +1,124 @@
 import os
 import table
 import util
+import re
+import shutil
+
+class SqlParser:
+
+    def __init__(self) -> None:
+        self.drop_head_pat = "drop"
+        self.drop_pat = r"[ ]*drop[ ]+table[ ]+([1-9a-zA-Z_]+)[ ]*;"
+        self.select_head_pat = "select"
+        self.select_pat = r"[ ]*select[ ]+([1-9a-zA-Z,_ \*]+)[ ]+from[ ]+([1-9a-zA-Z_]+)[ ]*;"
+        self.create_head_pat = "create"
+        self.create_pat = r"[ ]*create[ ]+table[ ]+([1-9a-zA-Z_]+)\(([1-9a-zA-Z, _]+)\)[ ]*;"
+
+    def parse(self, input_string):
+        input_string = input_string.strip().lower()
+
+        r = re.match(self.drop_head_pat, input_string)
+        if r:
+            r_ = re.search(self.drop_pat, input_string)
+            if r_:
+                tbName = r_.group(1)
+                dropTable(tbName)
+            else:
+                print("syntax error: drop table tablename;")
+
+        r = re.match(self.select_head_pat, input_string)
+        if r:
+            r_ = re.search(self.select_pat, input_string)
+            if r_:
+                colNamesText = r_.group(1)
+                colNames = colNamesText.strip().split(",")
+                colNames = [i.strip() for i in colNames]
+                # print(colNames)
+                tbName = r_.group(2)
+                # print(tbName)
+                tmpT = getColumn(tbName, colNames)
+                if tmpT:
+                    tmpT.showTable()
+            else:
+                print("syntax error: select colnames from tablename;")
+
+        r = re.match(self.create_head_pat, input_string)
+        if r:
+            r_ = re.search(self.create_pat, input_string)
+            if r_:
+                tbName = r_.group(1)
+                colNames = r_.group(2).strip().split(",")
+                colNames = [i.strip() for i in colNames]
+                tmpCols = colNames[:]
+                colNames = []
+                for c in tmpCols:
+                    tmp_ = c.split(" ")
+                    tmp_ = [i.strip() for i in tmp_]
+                    tmp_ = list(filter(lambda x: x and x.strip(), tmp_))
+                    tmp_ = " ".join(tmp_)
+                    colNames.append(tmp_)
+                ret = createTable(tbName, colNames)
+                if ret:
+                    print("CREATE TABLE {}".format(tbName))
+                else:
+                    print("CREATE DATABASE FAILED")
+            else:
+                print("syntax error: create table tablename(col1 type1, col2 type2, ...);")
+
+def createTable(tbName, colNames):
+    tablePathBase = os.path.join(util.getHomeDir(), "storage")
+    tableDirPath = os.path.join(tablePathBase, tbName)
+
+    if os.path.exists(tableDirPath):
+        print("table {} already exixts".format(tbName))
+        return False
+    else:
+        os.mkdir(tableDirPath)
+        tableMetaPath = os.path.join(tableDirPath, tbName+".meta")
+        with open(tableMetaPath, "w") as f:
+            f.writelines([i+"\n" for i in colNames])
+
+        for c in colNames:
+            colName = c.split(" ")[0]
+            colPath = os.path.join(tableDirPath, colName+".wcol")
+            with open(colPath, "w") as f:
+                pass
+
+        return True
+    
+def dropTable(tbName):
+    tablePathBase = os.path.join(util.getHomeDir(), "storage")
+    tableDirPath = os.path.join(tablePathBase, tbName)
+    if os.path.exists(tableDirPath):
+        shutil.rmtree(tableDirPath)
+
+        print("TABLE {} DROPED".format(tbName))
+    else:
+        print("TABLE {} NOT EXISTS".format(tbName))
 
 def getColumn(tbName, colNames):
     tablePathBase = os.path.join(util.getHomeDir(), "storage")
-    tablePath = os.path.join(tablePathBase, tbName+".wtbl")
+    tableDirPath = os.path.join(tablePathBase, tbName)
+    tableMetaPath = os.path.join(tableDirPath, tbName+".meta")
     # undone
-    if os.path.exists(tablePath):
-        tmpT = table.Table(tbName)
-        with open(tablePath, 'r') as f:
-            lines = f.readlines()
-            cols = [i.strip().split(",")[0] for i in lines]
-            ind = 0
-            for c in colNames:
-                if c in cols:
-                    colFilePath = os.path.join(tablePathBase, c+".wcol")
-                    if os.path.exists(colFilePath):
-                        tmpT.addColumn(c)
-                        with open(colFilePath, 'r') as f:
-                            lines = f.readlines()
-                            colDatas = [i.strip() for i in lines]
-                            tmpT.addColumnData(ind, colDatas)
-                        ind += 1
-                    else:
-                        print("get column data error")
-                elif c == "*":
-                    for c_ in cols:
-                        colFilePath = os.path.join(tablePathBase, c_+".wcol")
+    if os.path.exists(tableDirPath):
+        if os.path.exists(tableMetaPath):
+            tmpT = table.Table(tbName)
+            with open(tableMetaPath, 'r') as f:
+                lines = f.readlines()
+                cols = [i.strip().split(" ")[0] for i in lines]
+
+                for c in colNames:
+                    if c not in cols and c != "*":
+                        print("column {} not exists in table {}".format(c, tbName))
+                        return
+
+                ind = 0
+                for c in colNames:
+                    if c in cols:
+                        colFilePath = os.path.join(tableDirPath, c+".wcol")
                         if os.path.exists(colFilePath):
-                            tmpT.addColumn(c_)
+                            tmpT.addColumn(c)
                             with open(colFilePath, 'r') as f:
                                 lines = f.readlines()
                                 colDatas = [i.strip() for i in lines]
@@ -36,7 +126,21 @@ def getColumn(tbName, colNames):
                             ind += 1
                         else:
                             print("get column data error")
-        return tmpT
+                    elif c == "*":
+                        for c_ in cols:
+                            colFilePath = os.path.join(tableDirPath, c_+".wcol")
+                            if os.path.exists(colFilePath):
+                                tmpT.addColumn(c_)
+                                with open(colFilePath, 'r') as f:
+                                    lines = f.readlines()
+                                    colDatas = [i.strip() for i in lines]
+                                    tmpT.addColumnData(ind, colDatas)
+                                ind += 1
+                            else:
+                                print("get column data error")
+            return tmpT
+        else:
+            print("table {} file damaged".format(tbName))
     else:
         print("table {} not exists".format(tbName))
 
