c320ed6a6618b3859eb1a705ef4edbc774013007
luwei <1261579359@qq.com>
2024-11-17 10:55:43 +0800

    修改一下结构

--- parser.py
+++ parser.py
@@ -2,7 +2,6 @@ import os
 import table
 import util
 import re
-import shutil
 import pandas as pd
 
 STRING_PAT = r"^'(.*)'$"
@@ -36,7 +35,7 @@ class SqlParser:
             r_ = re.search(self.drop_pat, input_string)
             if r_:
                 tbName = r_.group(1)
-                dropTable(tbName)
+                util.dropTable(tbName)
             else:
                 print("syntax error: drop table tablename;")
 
@@ -51,7 +50,7 @@ class SqlParser:
                 # print(colNames)
                 tbName = r_.group(2)
                 # print(tbName)
-                tmpT = getColumn(tbName, colNames)
+                tmpT = util.getColumn(tbName, colNames)
                 if tmpT:
                     tmpT.showTable()
             else:
@@ -73,7 +72,7 @@ class SqlParser:
                     tmp_ = list(filter(lambda x: x and x.strip(), tmp_))
                     tmp_ = " ".join(tmp_)
                     colNames.append(tmp_)
-                ret = createTable(tbName, colNames)
+                ret = util.createTable(tbName, colNames)
                 if ret:
                     print("CREATE TABLE {}".format(tbName))
                 else:
@@ -91,7 +90,7 @@ class SqlParser:
                 values = r_.group(2).strip().split(",")
                 values = [i.strip() for i in values]
                 
-                insertValues(tbName, values)
+                util.insertValues(tbName, values)
 
             else:
                 print("syntax error: insert into tablename values(val1, val2, ...);")
@@ -167,231 +166,6 @@ class SqlParser:
 
         if not match_flag:
             print("syntax error, match nothing")
-
-def insertValues(tbName, values):
-    tablePathBase = os.path.join(util.getHomeDir(), "storage")
-    tableDirPath = os.path.join(tablePathBase, tbName)
-
-    if os.path.exists(tableDirPath):
-        tableMetaPath = os.path.join(tableDirPath, tbName+".meta")
-        if os.path.exists(tableMetaPath):
-            with open(tableMetaPath, 'r') as f:
-                lines = f.readlines()
-                cols = [i.strip().split(" ")[0] for i in lines]
-                if len(cols) != len(values):
-                    print("column not match")
-                    return
-                
-                types = [i.strip().split(" ")[1] for i in lines]
-                
-                len_types = len(types)
-
-                values_parsed = []
-
-                for i in range(len_types):
-                    if types[i].lower() == "string":
-                        v = re.search(STRING_PAT, values[i])
-                        if v:
-                            values_parsed.append(v.group(1))
-                        else:
-                            print("value '{}' parse failed".format(values[i]))
-                            return
-                    
-                    if types[i].lower() == "int":
-                        v = re.search(INT_PAT, values[i])
-                        if v:
-                            values_parsed.append(int(v.group(1)))
-                        else:
-                            print("value '{}' parse failed".format(values[i]))
-                            return
-
-                    if types[i].lower() == "double":
-                        v = re.search(DOUBLE_PAT_1, values[i])
-                        if v:
-                            values_parsed.append(float(v.group(1)))
-                        else:
-                            v = re.search(DOUBLE_PAT_2, values[i])
-                            if v:
-                                values_parsed.append(float(v.group(1)))
-                            else: 
-                                print("value '{}' parse failed".format(values[i]))
-                                return
-
-                values = values_parsed
-                
-                len_cols = len(cols)
-                for i in range(len_cols):
-                    colFilePath = os.path.join(tableDirPath, cols[i]+".wcol")
-                    with open(colFilePath, 'ab') as fout:
-                        if types[i].lower() == "string":
-                            d_ = util.string_to_fixed_bytes(values[i], 255)
-                            fout.write(d_)
-
-                        if types[i].lower() == "int":
-                            d_ = util.int_to_fixed_bytes(values[i])
-                            fout.write(d_)
-
-                        if types[i].lower() == "double":
-                            d_ = util.double_to_fixed_bytes(values[i])
-                            fout.write(d_)
-
-
-        else:
-            print("table {} file damaged".format(tbName))
-    else:
-        print("table {} not exists".format(tbName))
-
-def createTable(tbName, colNames):
-    tablePathBase = os.path.join(util.getHomeDir(), "storage")
-    tableDirPath = os.path.join(tablePathBase, tbName)
-
-    if not os.path.exists(tablePathBase):
-        os.mkdir(tablePathBase)
-
-    for c in colNames:
-        colType = c.split(" ")[1]
-        if colType.lower() not in ["int", "string", "double"]:
-            print("Only support INT, STRING and DOUBLE .")
-            return False
-
-    if os.path.exists(tableDirPath):
-        print("table {} already exixts".format(tbName))
-        return False
-    else:
-        os.mkdir(tableDirPath)
-        tableMetaPath = os.path.join(tableDirPath, tbName+".meta")
-        with open(tableMetaPath, "w") as f:
-            f.writelines([i+"\n" for i in colNames])
-
-        for c in colNames:
-            colName = c.split(" ")[0]
-            colPath = os.path.join(tableDirPath, colName+".wcol")
-            with open(colPath, "w") as f:
-                pass
-
-        return True
-    
-def dropTable(tbName):
-    tablePathBase = os.path.join(util.getHomeDir(), "storage")
-    tableDirPath = os.path.join(tablePathBase, tbName)
-    if os.path.exists(tableDirPath):
-        shutil.rmtree(tableDirPath)
-
-        print("TABLE {} DROPED".format(tbName))
-    else:
-        print("TABLE {} NOT EXISTS".format(tbName))
-
-def getColumn(tbName, colNames):
-    tablePathBase = os.path.join(util.getHomeDir(), "storage")
-    tableDirPath = os.path.join(tablePathBase, tbName)
-    tableMetaPath = os.path.join(tableDirPath, tbName+".meta")
-    # undone
-    if os.path.exists(tableDirPath):
-        if os.path.exists(tableMetaPath):
-            tmpT = table.Table(tbName)
-            with open(tableMetaPath, 'r') as f:
-                lines = f.readlines()
-                cols = [i.strip().split(" ")[0] for i in lines]
-                types = [i.strip().split(" ")[1] for i in lines]
-
-                for c in colNames:
-                    if c not in cols and c != "*":
-                        print("column {} not exists in table {}".format(c, tbName))
-                        return
-
-                ind = 0
-                for c in colNames:
-                    if c in cols:
-                        colFilePath = os.path.join(tableDirPath, c+".wcol")
-                        if os.path.exists(colFilePath):
-                            tmpT.addColumn(c)
-                            with open(colFilePath, 'rb') as f:
-                                index = cols.index(c)
-                                tp = types[index]
-
-                                colDatas = []
-                                if tp.lower() == "string":
-                                    len = int(f.seek(0, 2) / 255)
-                                    f.seek(0)
-
-                                    for j in range(len):
-                                        s = f.read(255)
-                                        len_head = int.from_bytes(s[:2], "big")
-                                        s = s[2:len_head+2]
-                                        d = s.decode(encoding='UTF-8',errors='strict')
-                                        colDatas.append(d)
-
-                                if tp.lower() == "int":
-                                    len = int(f.seek(0, 2) / 4)
-                                    f.seek(0)
-
-                                    for j in range(len):
-                                        s = f.read(4)
-                                        d = util.int_from_fixed_bytes(s)
-                                        colDatas.append(d)
-
-                                if tp.lower() == "double":
-                                    len = int(f.seek(0, 2) / 8)
-                                    f.seek(0)
-
-                                    for j in range(len):
-                                        s = f.read(8)
-                                        d = util.double_from_fixed_bytes(s)
-                                        colDatas.append(d)
-
-                                tmpT.addColumnData(ind, colDatas)
-                            ind += 1
-                        else:
-                            print("get column data error")
-                    elif c == "*":
-                        # print(cols)
-                        for c_ in cols:
-                            colFilePath = os.path.join(tableDirPath, c_+".wcol")
-                            if os.path.exists(colFilePath):
-                                tmpT.addColumn(c_)
-                                with open(colFilePath, 'rb') as f:
-                                    index = cols.index(c_)
-                                    tp = types[index]
-
-                                    colDatas = []
-                                    if tp.lower() == "string":
-                                        len = int(f.seek(0, 2) / 255)
-                                        f.seek(0)
-
-                                        for j in range(len):
-                                            s = f.read(255)
-                                            len_head = int.from_bytes(s[:2], "big")
-                                            s = s[2:len_head+2]
-                                            d = s.decode(encoding='UTF-8',errors='strict')
-                                            colDatas.append(d)
-
-                                    if tp.lower() == "int":
-                                        len = int(f.seek(0, 2) / 4)
-                                        f.seek(0)
-
-                                        for j in range(len):
-                                            s = f.read(4)
-                                            d = util.int_from_fixed_bytes(s)
-                                            colDatas.append(d)
-
-                                    if tp.lower() == "double":
-                                        len = int(f.seek(0, 2) / 8)
-                                        f.seek(0)
-
-                                        for j in range(len):
-                                            s = f.read(8)
-                                            d = util.double_from_fixed_bytes(s)
-                                            colDatas.append(d)
-                                    # print(colDatas)
-                                    tmpT.addColumnData(ind, colDatas)
-                                ind += 1
-                            else:
-                                print("get column data error")
-            return tmpT
-        else:
-            print("table {} file damaged".format(tbName))
-    else:
-        print("table {} not exists".format(tbName))
 
 if __name__ == "__main__":
     pass
--- util.py
+++ util.py
@@ -1,6 +1,238 @@
 import os
 import table
 import struct
+import re
+import shutil
+
+STRING_PAT = r"^'(.*)'$"
+INT_PAT = r"^(\d+)$"
+DOUBLE_PAT_1 = r"^(\d+\.\d+)$"
+DOUBLE_PAT_2 = r"^(\d+)$"
+
+def createTable(tbName, colNames):
+    tablePathBase = os.path.join(getHomeDir(), "storage")
+    tableDirPath = os.path.join(tablePathBase, tbName)
+
+    if not os.path.exists(tablePathBase):
+        os.mkdir(tablePathBase)
+
+    for c in colNames:
+        colType = c.split(" ")[1]
+        if colType.lower() not in ["int", "string", "double"]:
+            print("Only support INT, STRING and DOUBLE .")
+            return False
+
+    if os.path.exists(tableDirPath):
+        print("table {} already exixts".format(tbName))
+        return False
+    else:
+        os.mkdir(tableDirPath)
+        tableMetaPath = os.path.join(tableDirPath, tbName+".meta")
+        with open(tableMetaPath, "w") as f:
+            f.writelines([i+"\n" for i in colNames])
+
+        for c in colNames:
+            colName = c.split(" ")[0]
+            colPath = os.path.join(tableDirPath, colName+".wcol")
+            with open(colPath, "w") as f:
+                pass
+
+        return True
+
+def insertValues(tbName, values):
+    tablePathBase = os.path.join(getHomeDir(), "storage")
+    tableDirPath = os.path.join(tablePathBase, tbName)
+
+    if os.path.exists(tableDirPath):
+        tableMetaPath = os.path.join(tableDirPath, tbName+".meta")
+        if os.path.exists(tableMetaPath):
+            with open(tableMetaPath, 'r') as f:
+                lines = f.readlines()
+                cols = [i.strip().split(" ")[0] for i in lines]
+                if len(cols) != len(values):
+                    print("column not match")
+                    return
+                
+                types = [i.strip().split(" ")[1] for i in lines]
+                
+                len_types = len(types)
+
+                values_parsed = []
+
+                for i in range(len_types):
+                    if types[i].lower() == "string":
+                        v = re.search(STRING_PAT, values[i])
+                        if v:
+                            values_parsed.append(v.group(1))
+                        else:
+                            print("value '{}' parse failed".format(values[i]))
+                            return
+                    
+                    if types[i].lower() == "int":
+                        v = re.search(INT_PAT, values[i])
+                        if v:
+                            values_parsed.append(int(v.group(1)))
+                        else:
+                            print("value '{}' parse failed".format(values[i]))
+                            return
+
+                    if types[i].lower() == "double":
+                        v = re.search(DOUBLE_PAT_1, values[i])
+                        if v:
+                            values_parsed.append(float(v.group(1)))
+                        else:
+                            v = re.search(DOUBLE_PAT_2, values[i])
+                            if v:
+                                values_parsed.append(float(v.group(1)))
+                            else: 
+                                print("value '{}' parse failed".format(values[i]))
+                                return
+
+                values = values_parsed
+                
+                len_cols = len(cols)
+                for i in range(len_cols):
+                    colFilePath = os.path.join(tableDirPath, cols[i]+".wcol")
+                    with open(colFilePath, 'ab') as fout:
+                        if types[i].lower() == "string":
+                            d_ = string_to_fixed_bytes(values[i], 255)
+                            fout.write(d_)
+
+                        if types[i].lower() == "int":
+                            d_ = int_to_fixed_bytes(values[i])
+                            fout.write(d_)
+
+                        if types[i].lower() == "double":
+                            d_ = double_to_fixed_bytes(values[i])
+                            fout.write(d_)
+
+
+        else:
+            print("table {} file damaged".format(tbName))
+    else:
+        print("table {} not exists".format(tbName))
+
+def dropTable(tbName):
+    tablePathBase = os.path.join(getHomeDir(), "storage")
+    tableDirPath = os.path.join(tablePathBase, tbName)
+    if os.path.exists(tableDirPath):
+        shutil.rmtree(tableDirPath)
+
+        print("TABLE {} DROPED".format(tbName))
+    else:
+        print("TABLE {} NOT EXISTS".format(tbName))
+
+def getColumn(tbName, colNames):
+    tablePathBase = os.path.join(getHomeDir(), "storage")
+    tableDirPath = os.path.join(tablePathBase, tbName)
+    tableMetaPath = os.path.join(tableDirPath, tbName+".meta")
+    # undone
+    if os.path.exists(tableDirPath):
+        if os.path.exists(tableMetaPath):
+            tmpT = table.Table(tbName)
+            with open(tableMetaPath, 'r') as f:
+                lines = f.readlines()
+                cols = [i.strip().split(" ")[0] for i in lines]
+                types = [i.strip().split(" ")[1] for i in lines]
+
+                for c in colNames:
+                    if c not in cols and c != "*":
+                        print("column {} not exists in table {}".format(c, tbName))
+                        return
+
+                ind = 0
+                for c in colNames:
+                    if c in cols:
+                        colFilePath = os.path.join(tableDirPath, c+".wcol")
+                        if os.path.exists(colFilePath):
+                            tmpT.addColumn(c)
+                            with open(colFilePath, 'rb') as f:
+                                index = cols.index(c)
+                                tp = types[index]
+
+                                colDatas = []
+                                if tp.lower() == "string":
+                                    len = int(f.seek(0, 2) / 255)
+                                    f.seek(0)
+
+                                    for j in range(len):
+                                        s = f.read(255)
+                                        len_head = int.from_bytes(s[:2], "big")
+                                        s = s[2:len_head+2]
+                                        d = s.decode(encoding='UTF-8',errors='strict')
+                                        colDatas.append(d)
+
+                                if tp.lower() == "int":
+                                    len = int(f.seek(0, 2) / 4)
+                                    f.seek(0)
+
+                                    for j in range(len):
+                                        s = f.read(4)
+                                        d = int_from_fixed_bytes(s)
+                                        colDatas.append(d)
+
+                                if tp.lower() == "double":
+                                    len = int(f.seek(0, 2) / 8)
+                                    f.seek(0)
+
+                                    for j in range(len):
+                                        s = f.read(8)
+                                        d = double_from_fixed_bytes(s)
+                                        colDatas.append(d)
+
+                                tmpT.addColumnData(ind, colDatas)
+                            ind += 1
+                        else:
+                            print("get column data error")
+                    elif c == "*":
+                        # print(cols)
+                        for c_ in cols:
+                            colFilePath = os.path.join(tableDirPath, c_+".wcol")
+                            if os.path.exists(colFilePath):
+                                tmpT.addColumn(c_)
+                                with open(colFilePath, 'rb') as f:
+                                    index = cols.index(c_)
+                                    tp = types[index]
+
+                                    colDatas = []
+                                    if tp.lower() == "string":
+                                        len = int(f.seek(0, 2) / 255)
+                                        f.seek(0)
+
+                                        for j in range(len):
+                                            s = f.read(255)
+                                            len_head = int.from_bytes(s[:2], "big")
+                                            s = s[2:len_head+2]
+                                            d = s.decode(encoding='UTF-8',errors='strict')
+                                            colDatas.append(d)
+
+                                    if tp.lower() == "int":
+                                        len = int(f.seek(0, 2) / 4)
+                                        f.seek(0)
+
+                                        for j in range(len):
+                                            s = f.read(4)
+                                            d = int_from_fixed_bytes(s)
+                                            colDatas.append(d)
+
+                                    if tp.lower() == "double":
+                                        len = int(f.seek(0, 2) / 8)
+                                        f.seek(0)
+
+                                        for j in range(len):
+                                            s = f.read(8)
+                                            d = double_from_fixed_bytes(s)
+                                            colDatas.append(d)
+                                    # print(colDatas)
+                                    tmpT.addColumnData(ind, colDatas)
+                                ind += 1
+                            else:
+                                print("get column data error")
+            return tmpT
+        else:
+            print("table {} file damaged".format(tbName))
+    else:
+        print("table {} not exists".format(tbName))
 
 def getHomeDir():
     return os.path.dirname(os.path.abspath(__file__))
@@ -23,7 +255,7 @@ def string_to_fixed_bytes(s, length):
     if len(s.encode('utf-8')) > length:
         raise ValueError("The string is longer than the specified length.")
     return struct.pack(">H", len(s.encode('utf-8'))) + s.encode('utf-8') + (' ' * (length - len(s.encode('utf-8')))).encode('utf-8')
- 
+
 def int_to_fixed_bytes(v):
     return struct.pack(">i", v)
 
